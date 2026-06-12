865873986cd74af4f0d5d50b5b1678162848ee5f
Biswajeet Mohapatra <mbiswajeet66@gmail.com>
2026-06-12 19:36:04 +0530

    Added the cards service for the microservice application

---
+++ Microservices/cards/src/main/java/com/microservices/cards/CardsApplication.java
@@ -0,0 +1,39 @@
+package com.microservices.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "EazyBank Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Biswajeet Mohapatra",
+						email = "mbiswajeet66@gmail.com",
+						url = "biswajeetmohapatra.vercel.app"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.eazybytes.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank loans microservice REST API documentations",
+				url = "http://localhost:9000/swagger-ui/index.html"
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
